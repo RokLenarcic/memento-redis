6ab2bd9eb7a3926d449c12d432da30bd25ee10fd
Rok Lenarcic <rok.lenarcic@gmail.com>
2022-02-18 16:57:47 +0100

    Use build tools

---
+++ build.clj
@@ -0,0 +1,31 @@
+(ns build
+  (:refer-clojure :exclude [test])
+  (:require [clojure.tools.build.api :as b] ; for b/git-count-revs
+            [org.corfield.build :as bb]))
+
+(def lib 'org.clojars.roklenarcic/memento-redis)
+(def version (format "0.1.%s" (b/git-count-revs nil)))
+
+(defn add-defaults [opts]
+  (assoc opts :lib lib :version version))
+
+(defn test "Run the tests." [opts]
+  (bb/run-tests opts))
+
+(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
+  (-> opts
+      add-defaults
+      (bb/run-tests)
+      (bb/clean)
+      (bb/jar)))
+
+(defn install "Install the JAR locally." [opts]
+  (-> opts
+      add-defaults
+      (bb/install)))
+
+(defn deploy "Deploy the JAR to Clojars." [opts]
+  (-> opts
+      (assoc :sign-releases? true)
+      add-defaults
+      (bb/deploy)))
